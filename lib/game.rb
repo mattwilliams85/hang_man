ed9ed13b75ccf6fd7800875f5d1ea03f5370cb28
Matthew Williams & sterling houghton <student@epicodus.com>
2014-07-30 11:33:01 -0700

    adds tests to string statements and comparing word arrays

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,55 @@
-require './word'
+# require './word'
 
 class Game
+  attr_writer :used_letters
+  attr_writer :word
+  attr_writer :hidden_word
+
   def initialize
     word = Word.new
-    @@word = word.generate
+    @word = word.generate.split('')
+    @used_letters = []
+    @counter = 0
   end
+
+  def hidden_word
+    @hidden_word = []
+    @word.length.times do
+    @hidden_word << "_"
+    end
+    @hidden_word
+  end
+
+  def game_word
+    @word
+  end
+
+  def letter(input)
+    snapshot = @hidden_word
+
+    if @used_letters.include?(input)
+      "You've already tried that letter"
+    else
+      @word.each_with_index do |x, index|
+        if input == x
+          @hidden_word[index] = x
+          @used_letters << x
+        end
+      end
+      if snapshot != @hidden_word
+        false
+      else
+        @hidden_word
+      end
+    end
+  end
+
 end
+
+# test_game = Game.new
+# test_game.hidden_word
+# test_game.game_word
+# print test_game.letter("a")
+
 
 
--- spec/hang_man_spec.rb
+++ spec/hang_man_spec.rb
@@ -3,13 +3,31 @@ require 'game'
 require 'word'
 
 describe('game') do
-  it('it initializes a new game with a random word') do
-    test_game = Game.new()
-    test_game.should be_an_instance_of Game
+  used = ['f','l','n']
+  word = ['b','m','w']
+  hidden = ['_','_','_']
+
+  it('initializes a new game with a random word') do
+    test_game = Game.new
+    expect(test_game).to be_an_instance_of(Game)
   end
-  it('it initializes a new game with a random word') do
-    test_game = Game.new()
-    test_game.should be_an_instance_of Game
+  it('creates an empty array the same length as random word') do
+    test_game = Game.new
+    expect(test_game.game_word.length).to eq(test_game.hidden_word.length)
+  end
+  it('returns a string statement if player uses the same letter more than once') do
+    test_game = Game.new
+    test_game.used_letters = used
+    test_game.word = word
+    test_game.hidden_word = hidden
+    expect(test_game.letter("f")).to eq("You've already tried that letter")
+  end
+  it('pushes the correctly guessed letter into the hidden-word array') do
+    test_game = Game.new
+    test_game.used_letters = used
+    test_game.word = word
+    test_game.hidden_word = hidden
+    expect(test_game.letter("m")).to eq(['_','m','_'])
   end
 end
 
